5fd56cec142c32015601c3a22434084716b75be0
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-11-06 12:00:25 -0700

    Refactor done

--- ListCalculations/Program.cs
+++ ListCalculations/Program.cs
@@ -8,20 +8,51 @@
             Console.WriteLine("Enter a list of numbers, separated by a comma:");
             var input = Console.ReadLine();
 
-            string[] splitInput = input.Split(',');
+            string[] splitInput = SplitOnComma(input);
 
+            var nums = ParseStringArrayToIntList(splitInput);
+
+            var smallNums = GetSmallestNums(nums, 4);
+
+            Console.WriteLine(SumNumbers(smallNums));
+        }
+
+        //turn input into array of strings.
+        static string[] SplitOnComma(string input)
+        {
+            return input.Split(',');
+        }
+
+        // convert each string in input to an integer, store as a list
+        static List<int> ParseStringArrayToIntList(string[] stringNumbers)
+        {
             List<int> nums = new List<int>();
 
-            foreach (var item in splitInput)
+            foreach (var item in stringNumbers)
             {
                 nums.Add(Convert.ToInt32(item));
             }
 
-            nums.Sort();
+            return nums;
+        }
 
-            var smallNums = nums.GetRange(0, 4);
+        // retrieve smallest numbers from list
+        static List<int> GetSmallestNums(List<int> allNums, int count)
+        {
+            allNums.Sort();
 
-            Console.WriteLine(smallNums.Sum());
+            return allNums.GetRange(0, count);
+        }
+
+        // sum small numbers
+        static int SumNumbers(List<int> nums)
+        {
+            return nums.Sum();
         }
     }
 }
+
+// Outputting and Getting information
+
+//3,15,21,-5,82,7,5
+
